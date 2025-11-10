55a006ee964097f31377c2e7a6832c21dc8afbef
Jamal Ait Chdid <jait-chd@c2r1p8.1337.ma>
2025-11-10 10:53:36 +0100

    after working on the project for about a month my peer reach this result and we enjoyed finishing the project together but sadly i assisst that i need to write the code from begining for the second time

---
+++ src/main.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jait-chd <jait-chd@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 14:59:43 by yabarhda          #+#    #+#             */
+/*   Updated: 2025/11/10 10:49:59 by jait-chd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/main.h"
+
+int	error_check(int ac, char **av)
+{
+	int	fd;
+	int	len;
+
+	if (ac != 2)
+		return (printf("Error\nNot enough arguments\n"), 0);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		return (printf("Error\nNo such file or directory\n"), 0);
+	len = ft_strlen(av[1]);
+	if (ft_strcmp(".cub", &av[1][len - 4]))
+		return (printf("Error\nInvalid file format\n"), close(fd), 0);
+	return (1);
+}
+
+static void	init_data(t_data *data)
+{
+	data->mlx = NULL;
+	data->map = NULL;
+	data->img = NULL;
+	data->img_addr = NULL;
+	data->size_line = 0;
+	data->bpp = 0;
+	data->endien = 0;
+	data->width = WIDTH;
+	data->height = HEIGHT;
+	data->map_array = NULL;
+	data->map_width = 0;
+	data->map_height = 0;
+	data->texture = ft_malloc(sizeof(t_texture));
+	data->texture->north = NULL;
+	data->texture->south = NULL;
+	data->texture->west = NULL;
+	data->texture->east = NULL;
+	data->texture->floor = NULL;
+	data->texture->ceiling = NULL;
+	data->player.x = 0;
+	data->player.y = 0;
+	data->player.angle = 0;
+	data->player.direction = 0;
+	data->player.dir_x = 0;
+	data->player.dir_y = 0;
+	data->player.plane_x = 0;
+	data->player.plane_y = 0;
+}
+
+int	main(int ac, char **av)
+{
+	t_data	*data;
+
+	if (!error_check(ac, av))
+		return (1);
+	data = ft_malloc(sizeof(t_data));
+	init_data(data);
+	parse_file(data, av[1]);
+	return (0);
+}
---
+++ src/parse/parse_file.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_file.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabarhda <yabarhda@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/30 15:17:00 by yabarhda          #+#    #+#             */
+/*   Updated: 2025/09/11 16:59:58 by yabarhda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/main.h"
+
+static void	final_check(t_data *data, int fd)
+{
+	if (!data->texture->north || !data->texture->west || !data->texture->east \
+		|| !data->texture->south || !data->texture->floor \
+		|| !data->texture->ceiling)
+	{
+		ft_malloc(-42);
+		ft_perror("Invalid texture file");
+		close(fd);
+		exit(1);
+	}
+}
+
+static void	check_texture_file(t_data *data, int f)
+{
+	int	fd;
+
+	fd = open(data->texture->north, O_RDONLY);
+	if (fd == -1)
+		(ft_malloc(-42), ft_perror("Invalid texture file"), close(f), exit(1));
+	close(fd);
+	fd = open(data->texture->west, O_RDONLY);
+	if (fd == -1)
+		(ft_malloc(-42), ft_perror("Invalid texture file"), close(f), exit(1));
+	close(fd);
+	fd = open(data->texture->south, O_RDONLY);
+	if (fd == -1)
+		(ft_malloc(-42), ft_perror("Invalid texture file"), close(f), exit(1));
+	close(fd);
+	fd = open(data->texture->east, O_RDONLY);
+	if (fd == -1)
+		(ft_malloc(-42), ft_perror("Invalid texture file"), close(f), exit(1));
+	close(fd);
+}
+
+static int	validate_elements(t_data *data)
+{
+	if (!data->texture->north || !data->texture->west || !data->texture->east \
+		|| !data->texture->south || !data->texture->floor \
+		|| !data->texture->ceiling)
+	{
+		return (0);
+	}
+	return (1);
+}
+
+void	parse_file(t_data *data, char *file)
+{
+	int		fd;
+	char	*line;
+	char	**arr_line;
+
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	if (!line)
+		(ft_malloc(-42), close(fd), ft_perror("Empty file"), exit(1));
+	while (line)
+	{
+		arr_line = ft_split(line, ' ');
+		if (arr_line[0][0] == '\n')
+		{
+			line = get_next_line(fd);
+			continue ;
+		}
+		check_element(data, arr_line, fd);
+		if (validate_elements(data))
+			break ;
+		line = get_next_line(fd);
+	}
+	final_check(data, fd);
+	check_texture_file(data, fd);
+	map_check(data, fd);
+}
